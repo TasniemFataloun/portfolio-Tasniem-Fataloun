1c0c4b1683b0d10721ed1ba0dd6b98ee10a4b119
Tasniem Fataloun <tasniemfa98@gmail.com>
2024-04-27 22:19:44 +0200

    fix start - links

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import Links from "./links/Links";
+import Links from "./Links/Links";
 import styles from "./Sidebar.module.css";
 import ToggleButton from "./ToggleButton/ToggleButton";
 
