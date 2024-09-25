7da57102bf9cd9569bf42396d3e479dafe83562a
TasniemFataloun <126420550+TasniemFataloun@users.noreply.github.com>
2024-09-26 00:11:18 +0300

    preloader removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import IdeeTwee from "./pages/ideeTwee/IdeeTwee";
 import ContactUs from "./pages/ContactUs/ContactUs";
 import About from "./pages/About/About";
-import Project from "./components/Projects/Project";
 import Projects3D from "./components/Projects3D/Projects3D";
-import Preloader from "./components/PreLoader/preLoader";
 
 function App() {
   return (
